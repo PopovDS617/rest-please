e6e9622ebbdccd01b358e6a2f079d9d7f849a49b
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-10-17 13:57:03 +0300

    Added custom colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './src/pages/**/*.{ts,tsx,js,jsx}'
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      customDarkBlue:'#1f363d',
+      customBlue:'#40798c',
+      customBlueGreen:'#70a9a1',
+      customGreen:'#9ec1a3',
+      customLightGreen:'#cfe0c3'
+    },
   },
   variants: {},
   plugins: [],
