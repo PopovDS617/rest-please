d16a2ee6f96475269247bb4be509b8fe19693666
PopovDS617 <popovds.bm@gmail.com>
2022-10-18 01:35:00 +0300

    added custom scrollbar, added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
-    './src/components/**/*.{ts,tsx,js,jsx}', 
-    './src/pages/**/*.{ts,tsx,js,jsx}'
+    './src/components/**/*.{ts,tsx,js,jsx}',
+    './src/pages/**/*.{ts,tsx,js,jsx}',
   ],
   theme: {
     colors: {
       transparent: 'transparent',
-      customDarkBlue:'#1f363d',
-      customBlue:'#40798c',
-      customBlueGreen:'#70a9a1',
-      customGreen:'#9ec1a3',
-      customLightGreen:'#cfe0c3'
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      customDarkBlue: '#15272d',
+      customBlue: '#40798c',
+      customBlueGreen: '#70a9a1',
+      customGreen: '#9ec1a3',
+      customLightGreen: '#cfe0c3',
+      customYellow: '#FFD460',
     },
   },
   variants: {},
   plugins: [],
-}
+};
